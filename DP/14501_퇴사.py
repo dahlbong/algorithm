4e24a3b840fcda20e1123a29d3a3b0f83e90eade
dahyun <halover111@gmail.com>
2025-03-18 20:16:40 +0900

    250318 실버3

---
+++ DP/14501_퇴사.py
@@ -0,0 +1,12 @@
+N = int(input())
+schedule = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [0] * (N+1)
+
+for i in range(N-1, -1, -1):
+    if i + schedule[i][0]  > N:
+        dp[i] = dp[i+1]
+    else: 
+        dp[i] = max(dp[i+1], dp[i + schedule[i][0]] + schedule[i][1])
+
+print(dp[0])
